96c1b32824d3b3b1c0d62570701ad0a492f22191
Dor Goldreer <dor.goldreer24082001@gmail.com>
2022-09-11 23:04:43 +0300

    Clean slate

--- 10_gdb/game.c
+++ 10_gdb/game.c
@@ -23,7 +23,7 @@ int main(void) {
     return EXIT_FAILURE;
   }
   printf("Correct! You win round1!\n");
-  
+
   int total = 0;
   for (int i = 0; i <= 5678; i++) {
     total = total ^ getOtherSN(i);
